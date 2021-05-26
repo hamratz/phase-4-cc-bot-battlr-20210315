61bf30407b1cf8a695983184ee51f08b4f130ce0
Marty Warfel <74479634+hamratz@users.noreply.github.com>
2021-05-26 15:01:23 -0500

    submission for deadline

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection"
+const botUrl = 'http://localhost:6001/bots'
 
 class BotsPage extends Component {
-  //start here with your code for step one
+  state = {
+    bots: [],
+    draftBots: [],
+  }
+
+  componentDidMount() {
+    fetch(botUrl)
+      .then(res => res.json())
+      .then(bots => this.setState({ bots }))
+      .then(console.log("bots fetched"))
+  }
+
+  draftBot = (bot) => {
+    this.setState({draftBots: [...this.state.draftBots, bot] }) 
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+
+    return ( 
+    <div>
+        
+        <BotCollection armyBot={this.draftBot}  bots={this.state.bots} />
+    </div>
+    )
   }
 }
 
